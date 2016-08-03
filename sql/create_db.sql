1562a78fb905fb2dcf9cee1c5c99a20763f6bfd0
pixelerazor <b.schauff@gmail.com>
2016-08-04 01:12:02 +0200

    Initial Commit

---
+++ sql/create_db.sql
@@ -0,0 +1,15 @@
+CREATE SCHEMA lulan DEFAULT CHARACTER SET utf8;
+CREATE TABLE lulan.users (
+  id INT NOT NULL,
+  nickName VARCHAR(90) NULL,
+  password VARCHAR(90) NULL,
+  firstName MEDIUMBLOB NULL,
+  lastName MEDIUMBLOB NULL,
+  PRIMARY KEY (id),
+  UNIQUE INDEX nickname_UNIQUE (nickName ASC));
+
+  CREATE TABLE lulan.games (
+    id INT NOT NULL,
+    gameName VARCHAR(90) NULL,
+    version VARCHAR(90) NULL,
+    PRIMARY KEY (id);

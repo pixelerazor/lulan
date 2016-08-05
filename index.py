ce52ea277295d0535bb0657ce85b253a0d7bf1ea
pixelerazor <b.schauff@gmail.com>
2016-08-06 00:50:38 +0200

    minor changes
    * Added alias in config
    * changed links in nav
    * added prototype code to index.py

--- index.py
+++ index.py
@@ -1,11 +1,21 @@
 #!/usr/bin/python3
+import os
+import re
 from fileOperations import getFileContent
 
 print("Content-Type: text/html\n")
+
 print(getFileContent("header.html"))
+"""uri = os.environ["REQUEST_URI"]
+site = re.findall("site=(\w*)", uri, re.ASCII)
+if site != []:
+    site = site[0]
+else:
+"""
 content = getFileContent("navigation.html")
 content2 = getFileContent("login.html")
 sp = content.split('<div id="">\n')
 print(('<div id="logbar">\n' + content2 + '\n').join(sp))
 print(getFileContent("registration.html"))
+
 print(getFileContent("footer.html"))

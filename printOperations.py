041e26ae9648527af257ad958e9aa04996691433
sreblac <sreblac@users.noreply.github.com>
2016-08-06 05:19:08 +0200

    Formatting
    * Fix indentation levels in html files.
    * Add indentation function.
    * Changed print functions to use correct indentation levels. Page source is now readable.

--- printOperations.py
+++ printOperations.py
@@ -1,17 +1,23 @@
 from fileOperations import getFileContent
 
-def printHeader():
+def indentNewlines(code, num=0):
+    sp = code.split("\n")
+    code = (num*"    ")+(("\n"+(num*"    ")).join(sp))
+    return code[0:-(num*4)]
+
+def printHeader(ind=0):
     print("Content-Type: text/html\n")
-    print(getFileContent("header.html"))
+    print("<!DOCTYPE html>")
+    print(getFileContent("header.html"), end='')
 
-def printNav():
+def printNav(ind=0):
     content = getFileContent("navigation.html")
-    content2 = getFileContent("login.html")
+    content2 = indentNewlines(getFileContent("login.html"), 2) # Add two indentations two match navgigation.html
     sp = content.split('<div id="">\n')
-    print(('<div id="logbar">\n' + content2 + '\n').join(sp))
+    print(indentNewlines(('<div id="logbar">\n' + content2).join(sp), 3), end='') # Add three indentations two match header.html
 
-def printSite():
-    print(getFileContent("registration.html"))
+def printSite(ind=0):
+    print(indentNewlines(getFileContent("registration.html"), 3), end='') # Add three indentations two match header.html
 
-def printFooter():
-    print(getFileContent("footer.html"))
+def printFooter(ind=0):
+    print(getFileContent("footer.html"), end='')

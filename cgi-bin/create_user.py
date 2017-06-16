8ced3ec9ac6469b3adda128234a0b012ea5b4c85
pixelerazor <b.schauff@gmail.com>
2017-06-17 01:50:17 +0200

    Created database connection with the registration form.

--- cgi-bin/create_user.py
+++ cgi-bin/create_user.py
@@ -59,7 +59,39 @@ if ("nickName" not in form) or ("password1" not in form) or ("password2" not in 
     print(content)
     printFooter()
 # Input valid, create the user
-# TODO: write to DB instead of HTML
+
+#Establish Database
+connection = sqlite3.connect("user.db")
+
+#Connect to database and set the cursor
+cursor = connection.cursor()
+
+#Create table in database
+sql_command = """
+CREATE TABLE user (
+userID INTEGER PRIMARY KEY,
+FIRSTNAME VARCHAR(30),
+LASTNAME VARCHAR(30),
+NICKNAME VARCHAR(30),
+GAMES VARCHAR(99999),
+PASSWORD VARCHAR(30),
+);"""
+
+#Execute tablecreation
+cursor.execute(sql_command)
+
+#Add new user to table
+sql.command = """INSERT INTO user
+(FIRSTNAME, LASTNAME, NICKNAME, GAMES, PASSWORD)
+VALUES ("""+form["firstName"].value+""", """+form["lastName"].value+""", """+form["nickName"].value+""", """+form["password1"].value+""");"""
+
+#Execute the user creation
+cursor.execute(sql_command)
+
+#Close connection to database
+connection.commit()
+connection.close()
+
 else:
     print("Content-Type: text/html\n")
     print("<!DOCTYPE html>")
--- cgi-bin/sqliteConnect.py
+++ cgi-bin/sqliteConnect.py
@@ -1,8 +1,11 @@
-import _sqlite3
+import sqlite3
+#Establish Database
 connection = sqlite3.connect("user.db")
 
+#Connect to database and set the cursor
 cursor = connection.cursor()
 
+#Create table in database
 sql_command = """
 CREATE TABLE user (
 userID INTEGER PRIMARY KEY,
@@ -12,12 +15,18 @@ NICKNAME VARCHAR(30),
 GAMES VARCHAR(99999),
 PASSWORD VARCHAR(30),
 );"""
+
+#Execute tablecreation
 cursor.execute(sql_command)
 
+#Add new user to table
 sql.command = """INSERT INTO user
 (FIRSTNAME, LASTNAME, NICKNAME, GAMES, PASSWORD)
-VALUES ("""firstName""", """lastName""", """nickName""", """games""", """password1""");"""
+VALUES ("""+form["firstName"].value+""", """+form["lastName"].value+""", """+form["nickName"].value+""", """+form["password1"].value+""");"""
+
+#Execute the user creation
 cursor.execute(sql_command)
 
+#Close connection to database
 connection.commit()
 connection.close()

b7a6cbd01e1c8a1cdc237ead4865e32665ca60a6
pixelerazor <b.schauff@gmail.com>
2017-06-21 19:48:23 +0200

    Established db connection to games.py

--- cgi-bin/games.py
+++ cgi-bin/games.py
@@ -4,18 +4,28 @@ import os
 import re
 from printOperations import *
 import cgi
+import sqlite3
 
 # Definitions that create the html page
 def games():
     global y
-    games = ["Act of Aggression", "Anno 1404", "Anno 1404: Venedig", "Anno 2205", "ARK: Survival Evolved", "Arma 2", "Arma 3", "Battlefield 1", "Battlefield 2", "Battlefield 3", "Battlefield 4", "Battlefield 1", "Borderlands", "Borderlands 2", "Call of Duty 4: Modern Warfare", "Civilisation 5", "Civilisation 6", "Company of Heros 2", "Counter-Strike 1.6", "Counter-Strike Source", "Counter-Strike: Global Offensive", "Day of Defeat: Source", "DayZ", "Diablo 3", "Divinity: Original Sin", "Doom", "Dota 2", "Evolve", "Factorio", "Far Cry 3", "Far Cry 4", "Grand theft Auto V", "Half-Life 2", "Hearthstone", "Helldivers", "Left 4 Dead", "Left 4 Dead 2", "Overwatch", "Path of Exile", "Payday 2", "Portal 2", "Project Cars", "Rust", "Star Citizen", "Star Wars: Battlefront", "Starcraft 2", "Team Fortress 2", "The Division", "The Elder Scrolls Online", "Total War: Attila", "Unreal Tournament 3", "War for the Overlord", "Wolfenstein: The new Order", "World of Warcraft"]
-    x = [g.replace(' ', '') for g in games]
-    y = [g.replace(':', '') for g in x]
-    for a, b in zip(y, games):
+    # Establish Database
+    connection = sqlite3.connect("cgi-bin/lulan.db")
+
+    # Connect to database and set the cursor
+    cursor = connection.cursor()
+
+    # Create table in database
+    games = cursor.execute("SELECT * FROM games;")
+
+
+    for uid, name in games:
         print("      <li>")
-        print('         <label><input type="checkbox" name="'+a+'" value="'+a+'">"'+b+'"<label>')
+        print('         <label><input type="checkbox" name="'+name+'" value="'+str(uid)+'">'+name+'<label>')
         print("      </li>")
-    return y
+    # Close connection to database
+    connection.commit()
+    connection.close()
 
 def gamesHtml():
     print("<form>")
@@ -36,15 +46,11 @@ def getFormData(y):
         print(searchterm)
 
 # Get index file for editing
-file = open("html/reg_games.html", "r")
-content = file.read()
-file.close()
-
-#printHeader()
-#printNav()
-#gamesHtml()
-#printFooter()
-getFormData(y)
+printHeader()
+printNav()
+gamesHtml()
+printFooter()
+#getFormData(y)
 
 #TODO: Solve the Problem with the missing file links
 #TODO: Maybe add a "Add games" button
